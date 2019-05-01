f91b2eef9b6146648c662c7f0d4f573bcfa2aea1
guilimadev <guilimadev@gmail.com>
2019-05-01 10:19:11 -0300

    Trabalho 1 01/05

--- projeto1/programa1/main.cpp
+++ projeto1/programa1/main.cpp
@@ -43,10 +43,22 @@ class Empresa {
 	std::vector<Funcionario> funcionarios;
 
 public:
+	Empresa(){
+
+	}
 	Empresa(string nome, string cnpj2){
 		name = nome;
 		cnpj = cnpj2;
 	}
+
+	void setNome(string n){
+		name = n;
+	}
+
+	void setCnpj(string c){
+			cnpj = c;
+		}
+
 	void add_func(Funcionario x){
 		for(std::vector<Funcionario>::const_iterator i = funcionarios.begin(); i != funcionarios.end(); i++){
 			
@@ -77,12 +89,55 @@ public:
 };
 
 int main(){
+	int opcao1;
+	std::vector<Empresa> e;
+	while(opcao1 != 3){
+		cout << endl << "Escolha a opcao: " << endl;
+		cout << "[1] - Criar Empresa" << endl;
+		cout << "[2] - Ver Empresas Criadas" << endl;
+		cout << "[3] - Fechar programa" << endl;
+		cin >> opcao1;
 	
+		if (opcao1 == 1)
+		{
+			Empresa temp;
+			string nametemp;
+			string cnpjtemp;
+	
+			cout << "Digite o nome da empresa: " << endl;
+			cin >> nametemp;
+			cout << "Digite o CNPJ da empresa: " << endl;
+			cin >> cnpjtemp;
+	
+			temp.setNome(nametemp);
+			temp.setCnpj(cnpjtemp);
+			
+			e.emplace_back(temp);
+
+			cout << "----------EMPRESA CRIADA-------" << endl;
+		}
+
+		else if (opcao1 == 2){
+			int z = 1;
+			cout << endl << "----- LISTA DE EMPRESAS -----" << endl;
+			for(auto i = e.begin(); i != e.end(); i++){
+				
+				cout << "Empresa " << z << " - "<< *i;
+				z++;
+			}
+			cout << "----- LISTA DE EMPRESAS -----" << endl;
+		}
+		else if(opcao1 != 1 or opcao1 != 2 or opcao1 != 3){
+			cout << "Entre um opcao valida!" << endl;
+		}
+	}
+
+	/*
 	Empresa a("Coca-Cola", "754.654.456/001-89");
 	Funcionario g;
 	Funcionario x("Pedro", 9090.6, "10073119407");
 	a.add_func(g);
 	a.add_func(x);
 	a.add_func(x);
-	cout << a;
+	cout << a; */
 }
